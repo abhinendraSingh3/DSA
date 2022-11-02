c05dd6edf168209608b81412e292cf69c5ee0583
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2022-11-02 11:07:26 +0530

    Create bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,7 @@
+list1=[23,12,43,65,1]
+for i in range(len(list1)-1):
+    for j in range(len(list1)-1):
+        if list1[j]>list1[j+1]:
+            #swapping
+            list1[j],list1[j+1]=list1[j+1],list1[j]
+print(list1)            
