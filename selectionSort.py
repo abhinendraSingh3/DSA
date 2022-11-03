d68424ff14649fbf608df19d94336dcb416b8625
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-11-03 15:21:33 +0530

    Added new method in selection sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,6 +1,22 @@
-list=[43,2,6,1,67,23,65]
+# list=[43,2,6,1,67,23,65]
+# for i in range(len(list)):
+#     minVal=min(list[i:])
+#     minInd=list.index(minVal,i)
+#     list[i],list[minInd]=list[minInd],list[i]
+# print(list) 
+
+#-----or------#
+list=[43,2,6,1,67,23,65]\
+
 for i in range(len(list)):
-    minVal=min(list[i:])
-    minInd=list.index(minVal,i)
-    list[i],list[minInd]=list[minInd],list[i]
-print(list)    
+    minVal=i
+
+    for j in range(i+1,len(list)):
+        if list[minVal]>list[j]:
+            minVal=j
+            
+    #swapping
+    list[i],list[minVal]=list[minVal],list[i]
+
+for i in range(len(list)):
+    print("%d" %list[i],end=" ")
