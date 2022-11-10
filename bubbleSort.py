3a128f40bc3f20ceb680909a0910b5aa1238a902
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2022-11-10 23:37:32 +0530

    Update bubbleSort.py

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,4 +4,4 @@ for i in range(len(list1)-1):
         if list1[j]>list1[j+1]:
             #swapping
             list1[j],list1[j+1]=list1[j+1],list1[j]
-print(list1)            
+print(list1)             
