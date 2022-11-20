4b996a387a868f172eb155f3808b1bfd6a15a709
Abhinendra Singh <75690933+abhinendraSingh3@users.noreply.github.com>
2022-11-20 12:57:18 +0530

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -9,3 +9,23 @@ for i in range(len(n)):
   else:
     l[j+1]=k
     print("sorted list",l)
+    
+    
+#--------------or--------------#
+
+
+def insertionSort(myList):
+    for i in range (1,len(myList)):
+        current_element=myList[i]
+        pos=i
+        while current_element<myList[pos-1] and pos>0:
+            myList[pos]=myList[pos-1]
+            pos=pos-1
+        myList[pos]=current_element
+    
+l1=[5,2,6,9,1]
+#if you want to take input from the user then 
+#n=int(input("enter some number"))
+#l1=list(n)
+insertionSort(l1)
+print(l1)
