4a6580cf994c92eeb484fc18eb7d60e9e7fe4d1a
Abhinendra Singh <abhinendrayadav3@gmail.com>
2022-11-30 15:53:06 +0530

    Create LL.java

---
+++ LL.java
@@ -0,0 +1,17 @@
+import org.w3c.dom.Node;
+
+class LL{
+class node{
+    String data;
+    Node next;
+
+    Node(String data){
+       this.data=data;
+       this.next=null;
+    }
+}
+    public static void main(String[] args) {
+       LL list=new LL();
+        
+    }
+}
